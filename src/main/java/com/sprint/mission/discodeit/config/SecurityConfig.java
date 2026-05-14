f4347f2e38061685d86e0f4b78eaa9a2c8b91bc2
zinuzanu <wsx104301@naver.com>
2026-05-14 10:10:28 +0900

    feat: Spring Security 환경 설정 및 관련 클래스 생성 및 설정

---
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.discodeit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.build();
+    }
+}
