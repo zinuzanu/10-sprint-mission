2696dd94e9454eb954c5ec03337d770b2c20cb5f
zinuzanu <wsx104301@naver.com>
2026-06-08 11:26:11 +0900

    feat: Redis Cache 도입 및 설정 적용

--- src/main/java/com/sprint/mission/discodeit/config/CacheConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/CacheConfig.java
@@ -1,34 +1,58 @@
 package com.sprint.mission.discodeit.config;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
-import java.util.concurrent.TimeUnit;
-import org.springframework.cache.CacheManager;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import java.time.Duration;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
 
   @Bean
-  public CacheManager cacheManager() {
-
-    CaffeineCacheManager cacheManager =
-        new CaffeineCacheManager(
-            "users",
-            "userChannels",
-            "userNotifications"
-        );
-
-    cacheManager.setCaffeine(
-        Caffeine.newBuilder()
-            .maximumSize(100)
-            .expireAfterAccess(600, TimeUnit.SECONDS)
-            .recordStats()
+  public RedisCacheConfiguration redisCacheConfiguration(ObjectMapper objectMapper) {
+    ObjectMapper redisObjectMapper = objectMapper.copy();
+    redisObjectMapper.activateDefaultTyping(
+        LaissezFaireSubTypeValidator.instance,
+        DefaultTyping.EVERYTHING,
+        As.PROPERTY
     );
 
-    return cacheManager;
+    return RedisCacheConfiguration.defaultCacheConfig()
+        .serializeValuesWith(
+            RedisSerializationContext.SerializationPair.fromSerializer(
+                new GenericJackson2JsonRedisSerializer(redisObjectMapper)
+            )
+        )
+        .prefixCacheNameWith("discodeit:")
+        .entryTtl(Duration.ofSeconds(600))
+        .disableCachingNullValues();
   }
+
+//  @Bean
+//  public CacheManager cacheManager() {
+//
+//    CaffeineCacheManager cacheManager =
+//        new CaffeineCacheManager(
+//            "users",
+//            "userChannels",
+//            "userNotifications"
+//        );
+//
+//    cacheManager.setCaffeine(
+//        Caffeine.newBuilder()
+//            .maximumSize(100)
+//            .expireAfterAccess(600, TimeUnit.SECONDS)
+//            .recordStats()
+//    );
+//
+//    return cacheManager;
+//  }
 }
